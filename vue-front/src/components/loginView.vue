2d387252a270fb198a938d3a2263a6e9cf0a7b06
JokerNoCry <liu1160860141@163.com>
2022-05-19 15:13:03 +0800

    编写前端界面

---
+++ vue-front/src/components/loginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>查询页面
+    <div v-for="item in user" :key="item.id">
+      <div> {{item.id}}    {{item.name}}    {{item.city}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginView",
+  created() {
+    this.$axios
+      .get("http://101.37.28.115:3000/user/all")
+      .then((response) => {
+        this.user = response.data.data;
+        console.log(this.user);
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+  },
+  data() {
+    return { user: [] };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
